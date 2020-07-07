cbfa126b4dd6db5c108f6942bae7b3baa0e15af1
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-07-07 11:33:32 -0300

    update react 16

---
+++ frontend/src/consts.js
@@ -0,0 +1,6 @@
+// mudar URL...
+export default {
+  API_URL: 'http://localhost:3003/api',
+  OAPI_URL: 'http://localhost:3003/oapi'
+}
+
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Router, Route, IndexRoute ,Redirect, hashHistory } from 'react-router'
 
-import App from './app'
-// import Dashboard2 from '../dashboard2/dashboard2'
+import { Switch, Route, Redirect } from 'react-router'
+
+import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycles/billingCycles'
 
 export default props => (
-  <Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={Dashboard} />
-      <Route path='billingCycles' component={BillingCycle} />
-    </Route>
-    <Redirect from='*' to='/' />
-  </Router>
+  <div className='content-wrapper'>
+    <Switch>
+      <Route exact path='/' component={Dashboard} />
+      <Route path='/billingCycles' component={BillingCycle} />
+      <Redirect from='*' to='/' />
+    </Switch>
+  </div>
 )
